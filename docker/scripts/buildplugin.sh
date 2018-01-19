2a25a50335a6fc30167e16efc35a7b26105442ff
M. Eric Irrgang <ericirrgang@gmail.com>
2018-01-19 13:47:01 -0500

    try to resolve build issues

--- docker/scripts/buildplugin.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p plugin-build && \
-(cd plugin-build && \
-gmxapi_DIR=/usr/local/gromacs cmake ../samplerestraint -DPYTHON_EXECUTABLE=`which python` && \
-LD_LIBRARY_PATH=/opt/conda/lib make && \
-make test) && \
-PYTHONPATH=plugin-build/src/pythonmodule python -m pytest samplerestraint/tests --verbose
--- docker/scripts/setuptoolsinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Build and install the gmx python module
-
-pip uninstall -y gmx
-
-(cd gmxpy && \
-gmxapi_DIR=/usr/local/gromacs python setup.py install --verbose && \
-gmxapi_DIR=/usr/local/gromacs python setup.py test)
