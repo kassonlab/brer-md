96636777e2fb95732b4c9791688d4b6ba2a81c75
M. Eric Irrgang <ericirrgang@gmail.com>
2018-03-16 15:33:40 -0500

    Make sure mpi check during tests matches test requirements.

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,7 @@ def cleandir():
 
 try:
     from mpi4py import MPI
-    withmpi_only = pytest.mark.skipif(not MPI.Is_initialized(),
-                                      reason="Test requires MPI, but MPI is not initialized or too small.")
+    withmpi_only = pytest.mark.skipif(not MPI.Is_initialized() or MPI.COMM_WORLD.Get_size() < 2,
+                                      reason="Test requires at least 2 MPI ranks, but MPI is not initialized or too small.")
 except ImportError:
-    withmpi_only = pytest.mark.skip(reason="Test requires at MPI, but mpi4py is not available.")
+    withmpi_only = pytest.mark.skip(reason="Test requires at least 2 MPI ranks, but mpi4py is not available.")
