74ca808b44d732c60d63c3d16865cce3e385d2a7
Jennifer M. Hays <jmh5sf@virginia.edu>
2018-08-15 17:12:56 -0400

    Store metadata in single json per pair. Some error checking.

--- src/run.py
+++ src/run.py
@@ -11,7 +11,7 @@ from src.resampler import *
 """
 
 # Initialize the state
-state_filename = "state.json"  # Where you want to store metadata
+state_filename = "../tests/state.json"  # Where you want to store metadata
 state = State()
 
 if os.path.exists(state_filename):
@@ -20,25 +20,39 @@ else:
     state.restart()
     state.write_to_json(state_filename)
 
+if not state.is_complete_record():
+    raise ValueError(
+        'You are missing parameters in your state.json file.\nProvided: {}\nRequires: {}'.
+        format(state.keys, state.required_keys))
+
 # Initialize the pair data
 # For now, I will read from jsons that contain the metadata for each pair.
 # The name from the json will be used as the names of each individual PairData object.
 # This could certainly be done differently.
 
 test_dir = '/home/jennifer/Git/run_brer/tests/'
-my_file_names = ['{}/052_210.json'.format(test_dir), '{}/105_216.json'.format(test_dir)]
+my_file_names = [
+    '{}/052_210.json'.format(test_dir), '{}/105_216.json'.format(test_dir)
+]
 
 data = [json.load(open(my_file_name, 'r')) for my_file_name in my_file_names]
 
 re_sampler = ReSampler()
 
 for single_pair_data in data:
-    name = single_pair_data['name']
-    pair_data = PairData(name=name)
-    pair_data.__setattr__('metadata', single_pair_data)
+    pair_data = PairData(name=single_pair_data['name'])
+    pair_data.load_metadata(single_pair_data)
     re_sampler.add_pair(pair_data)
 
-""" Get a new set of distributions if you are at the beginning of a simulation """
 
-if state.get('iteration') == 0 and state.get('phase') == 'training':
-    print(re_sampler.get_names())
+# Get a new set of distributions if you are at the beginning of a simulation
+# Otherwise, grab the old targets.
+if state.get('phase') == 'training':
+    targets = re_sampler.resample()
+# else:
+#     targets = state.get('targets')
+if state.get('phase') == 'convergence':
+    pass
+else:
+    pass
+
