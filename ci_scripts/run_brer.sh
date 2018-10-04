d7b8551afd0c2ab2f0d301322066fd9ab1bd6916
Jennifer M. Hays <jmh5sf@virginia.edu>
2018-10-04 09:10:31 -0400

    Add Travis CI

---
+++ ci_scripts/run_brer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ev
+
+pushd $HOME
+ [ -d run_ebmetad ] || git clone --depth=1 --no-single-branch https://github.com/jmhays/run_ebmetad.git
+ pushd run_brer
+  git branch -a
+  if [ "${TRAVIS_BRANCH}" != "master" ] ; then
+   git checkout devel
+  else
+   git checkout master
+  fi
+  $PYTHON setup.py install
+  PYTHONPATH=../sample_restraint/build/src/pythonmodule $PYTHON -m pytest --cov=./
+ popd
+popd
