3386b41f0d874cc4aa94b0f5b9e4c7c6cac95b21
Jennifer M. Hays <jmh5sf@virginia.edu>
2018-10-09 12:56:43 -0400

    Again, don't popd out of run_brer

--- ci_scripts/run_brer.sh
+++ ci_scripts/run_brer.sh
@@ -13,4 +13,3 @@ pushd $HOME
   $PYTHON setup.py install
   # PYTHONPATH=$HOME/sample_restraint/build/src/pythonmodule $PYTHON -m pytest --cov=./run_brer
   pytest --cov=run_brer --pyargs run_brer
- popd
