5b34d12e05126337f3fe6d3732fc932b8529d960
Jennifer M. Hays <jmh5sf@virginia.edu>
2019-07-03 10:12:15 -0400

    add versioneer, allow pip install

---
+++ run_brer/__init__.py
@@ -0,0 +1,13 @@
+"""
+run_brer
+"""
+
+# Add imports here
+import run_brer 
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
--- setup.py
+++ setup.py
@@ -1,45 +1,59 @@
-import setuptools
-import os
+"""
+run_brer
+"""
+
+import sys
+from setuptools import setup, find_packages
+import versioneer
+
+short_description = __doc__.split("\n")
+
+# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:]),
 
 
-package_dir = os.path.abspath('src')
+setup(
+    # Self-descriptive entries which should always be present
+    name='run_brer',
+    author='Jennifer M. Hays',
+    author_email='jennifer.m.hays@gmail.com',
+    description=short_description[0],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='LGPLv2.1',
 
+    # Which Python importable modules should be included when your package is installed
+    # Handled automatically by setuptools. Use 'exclude' to prevent some specific
+    # subpackage(s) from being added, if needed
+    packages=find_packages(),
 
-if __name__ == "__main__":
-    setuptools.setup(
-        name='run_brer',
-        version='0.0.1',
-        description='Package to run BRER simulations',
-        author='Jennifer Hays',
-        author_email='jmh5sf@virginia.edu',
-        url="https://github.com/jmhays/run_brer",
-        license='LGPL-2.1',
-        packages=setuptools.find_packages(),
-        install_requires=[],
-        extras_require={
-            'docs': [
-                'sphinx',
-                'sphinxcontrib-napoleon',
-                'sphinx_rtd_theme',
-                'numpydoc',
-            ],
-            'tests': [
-                'pytest',
-                'pytest-cov',
-                'pytest-pep8',
-                'tox',
-            ],
-        },
-        tests_require=[
-            'pytest',
-            'pytest-cov',
-            'pytest-pep8',
-            'tox',
-        ],
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Science/Research',
-            'Programming Language :: Python :: 3',
-        ],
-        zip_safe=False,
-    )
+    # Optional include package data to ship with your package
+    # Customize MANIFEST.in if the general case does not suit your needs
+    # Comment out this line to prevent the files from being packaged with your software
+    include_package_data=True,
+
+    # Allows `setup.py test` to work correctly with pytest
+    setup_requires=[] + pytest_runner,
+
+    # Additional entries you may want simply uncomment the lines you want and fill in the data
+    # url='http://www.my_package.com',  # Website
+    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    # platforms=['Linux',
+    #            'Mac OS-X',
+    #            'Unix',
+    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
+    # python_requires=">=3.5",          # Python version restrictions
+
+    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
+    # zip_safe=False,
+
+)
