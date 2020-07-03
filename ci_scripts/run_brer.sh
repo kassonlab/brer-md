736325742bacb946f7d3df9d2926e6907542d9df
M. Eric Irrgang <ericirrgang@gmail.com>
2020-07-03 12:29:27 +0300

    Update Travis-CI config (#5)
    YAML schema has changed a bit, and we can use a more recent Linux image.
    
    * Update Travis-CI config
    * Refine Travis-CI scripts.
    * Use the `brer` branch of the plugin for self-consistency with `run_brer:master`

--- ci_scripts/run_brer.sh
+++ ci_scripts/run_brer.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -ev
 
-pushd $HOME
- [ -d run_brer ] || git clone --depth=1 --no-single-branch https://github.com/jmhays/run_brer.git
- pushd run_brer
-  git branch -a
-  if [ "${TRAVIS_BRANCH}" != "master" ] ; then
-   git checkout devel
-  else
-   git checkout master
-  fi
-  $PYTHON setup.py install
-  # PYTHONPATH=$HOME/sample_restraint/build/src/pythonmodule $PYTHON -m pytest --cov=./run_brer
-  pytest --cov=run_brer --pyargs run_brer
- popd
-popd
+$PYTHON setup.py install
+# PYTHONPATH=$HOME/sample_restraint/build/src/pythonmodule $PYTHON -m pytest --cov=./run_brer
+$PYTHON -m pytest --cov=run_brer --pyargs run_brer -s
