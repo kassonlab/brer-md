kassonlab/brer-md

commit 46157f98557b84091b8e2c729580b04d1f16b6f3

add more logging for mysteriously unobserved checkpoint files.

run_brer/run_config.py
@@ -133,7 +133,7 @@ class RunConfig:
 
         def safe_copy(src, dst):
             if not os.path.exists(src):
-                raise RuntimeError('Missing checkpoint file from previous iteration: {}'.format(src))
+                raise RuntimeError('Missing file: {}'.format(src))
             if os.path.exists(dst):
                 raise RuntimeError('Destination file already exists: {}'.format(dst))
             size = os.stat(src).st_size
@@ -181,6 +181,8 @@ class RunConfig:
                 # Get the convergence cpt from current iteration
                 source = '{}/{}/convergence/state.cpt'.format(member_dir, current_iter)
                 if not os.path.exists(source):
+                    self._logger.error(f'os.path.exists({source}) is False! Getting directory listing.')
+                    self._logger.error(str(os.path.listdir(os.path.dirname(source))))
                     raise RuntimeError('Missing checkpoint file from convergence phase: {}'.format(source))
                 safe_copy(source, target)
 

commit 43318687f9dd05a80f723e50634c4a0df643b34f

Fix typo.
Fix typo introduced in 46157f98

run_brer/run_config.py
@@ -182,7 +182,7 @@ class RunConfig:
                 source = '{}/{}/convergence/state.cpt'.format(member_dir, current_iter)
                 if not os.path.exists(source):
                     self._logger.error(f'os.path.exists({source}) is False! Getting directory listing.')
-                    self._logger.error(str(os.path.listdir(os.path.dirname(source))))
+                    self._logger.error(str(os.listdir(os.path.dirname(source))))
                     raise RuntimeError('Missing checkpoint file from convergence phase: {}'.format(source))
                 safe_copy(source, target)
 