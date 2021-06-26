548dcefd684531378452df465437e43daeb23368
M. Eric Irrgang <ericirrgang@gmail.com>
2021-06-26 12:45:52 +0300

    Update versioneer.
    Reset _version.py and versioneer.py to distributed version, plus three years.

--- run_brer/__init__.py
+++ run_brer/__init__.py
@@ -4,8 +4,6 @@ run_brer
 
 # Handle versioneer
 from ._version import get_versions
-
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+__version__ = get_versions()['version']
+__git_revision__ = get_versions()['full-revisionid']
+del get_versions
