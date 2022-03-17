a5188d2b41083f01079e43a96871e4e45f98da7f
M. Eric Irrgang <ericirrgang@gmail.com>
2022-03-17 20:26:05 +0300

    Update older build/install scripts.

--- ci_scripts/install_gromacs_2019.sh
+++ ci_scripts/install_gromacs_2019.sh
@@ -2,34 +2,36 @@
 set -ev
 
 export GMX_DOUBLE=OFF
+export GMX_MPI=OFF
+export GMX_THREAD_MPI=ON
 
-export GMX_SRC_DIR=gromacs-2019
+export GMX_SRC_DIR=gromacs-release-2019
 
-export CCACHE_DIR=$HOME/.ccache_gmxapi
 ccache -s
 
 pushd $HOME
- [ -d gromacs-gmxapi ] || \
+ [ -d ${GMX_SRC_DIR} ] || \
     git clone \
         --depth=1 \
         -b release-2019 \
-        https://github.com/gromacs/gromacs.git \
+        https://gitlab.com/gromacs/gromacs.git \
         ${GMX_SRC_DIR}
  pushd ${GMX_SRC_DIR}
   pwd
   rm -rf build
   mkdir build
   pushd build
-   cmake -DCMAKE_CXX_COMPILER=$CXX \
+   cmake -G Ninja \
+         -DCMAKE_CXX_COMPILER=$CXX \
          -DGMX_ENABLE_CCACHE=ON \
          -DCMAKE_C_COMPILER=$CC \
          -DGMX_DOUBLE=$GMX_DOUBLE \
          -DGMX_MPI=$GMX_MPI \
          -DGMX_THREAD_MPI=$GMX_THREAD_MPI \
          -DGMXAPI=ON \
-         -DCMAKE_INSTALL_PREFIX=$HOME/install/gromacs_2019 \
+         -DCMAKE_INSTALL_PREFIX=$HOME/install/gromacs-release-2019 \
          ..
-   make -j2 install
+   cmake --build . --target install
   popd
  popd
 popd
