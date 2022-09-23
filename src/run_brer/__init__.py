c27996e1ca8a99f66648cabb07457478160b0818
M. Eric Irrgang <ericirrgang@gmail.com>
2022-09-23 13:48:09 +0300

    Tidying.
    Clean up configs. Resolve deprecation warnings and other minor issues.

--- src/run_brer/__init__.py
+++ src/run_brer/__init__.py
@@ -1,6 +1,14 @@
 """A set of scripts for running BRER simulations using gmxapi.
 
-run_brer
+Online documentation is hosted at https://kassonlab.github.io/run_brer/.
+
+See Also
+--------
+Hays, J. M., Cafiso, D. S., & Kasson, P. M.
+Hybrid Refinement of Heterogeneous Conformational Ensembles using Spectroscopic Data.
+*The Journal of Physical Chemistry Letters.*
+DOI: `10.1021/acs.jpclett.9b01407 <https://pubs.acs.org/doi/10.1021/acs.jpclett.9b01407>`__
 """
+__all__ = ['__version__']
 
 from ._version import __version__
